42931b9c387ed13b8219696247846d408891fd0b
vlad <vlad.gromov666@gmail.com>
2023-11-14 17:02:48 +0300

    2023.11.14 - base structure app

---
+++ main/config.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Config:
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    prod=ProductionConfig
+)
